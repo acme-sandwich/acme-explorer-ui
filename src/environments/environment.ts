e6bef3edbcc22e1530f1c623757cbc24b87528ba
arodriguez <arodriguez@bebeyond.es>
2020-03-30 13:56:14 +0200

    Changed api backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  backendApiBaseURL: "http://api.ec2-52-207-253-241.compute-1.amazonaws.com"
+  //backendApiBaseURL: "http://api.ec2-52-207-253-241.compute-1.amazonaws.com"
+  backendApiBaseURL: "http://localhost:3000"
 };
 
 /*
